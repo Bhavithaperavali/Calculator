e7d849211d3fe551982c6402d1845a59b986aae7
Bhavithaperavali <bhavithaperavali2005@gmail.com>
2025-07-05 11:54:18 +0530

    Add files via upload

---
+++ calculator/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home(request):
+    result=None
+    if request.method == 'POST':
+        a=int(request.POST.get('num1'))
+        b=int(request.POST.get('num2'))
+        o=request.POST.get('op')
+        if o == 'add':
+            result=a+b
+        
+        #return render(request,'index.html',['result':result])
+        return redirect('hello',result)
+    return render(request,'index.html')
+
+def hello(request,result):
+    return render(request,'result.html',{'result':result})
